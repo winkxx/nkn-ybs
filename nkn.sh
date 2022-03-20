e98e3df99c20d3c62a90a3824c5e94fb84a116a8
winkxx <68945988+winkxx@users.noreply.github.com>
2022-03-20 23:06:07 +0800

    Update nkn.sh

--- nkn.sh
+++ nkn.sh
@@ -1,4 +1,4 @@
 wget https://commercial.nkn.org/downloads/nkn-commercial/linux-amd64.zip
 unzip linux-amd64.zip
 cd linux-amd64
-./nkn-commercial -b 你的地址 install
+./nkn-commercial -b NKNFSveT77regjv5k5QjFhD9nAPk6sZWxBHT install
